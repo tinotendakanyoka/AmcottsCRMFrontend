ed82b8fbab71e5e5c21b27d5d079c82aa9eea273
Tinotenda Kanyoka <purplelaneofficial@gmail.com>
2026-08-17 19:35:32 +0200

    CSS files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,10 @@
-<script setup></script>
+<script setup>
+import AppLayout from './layouts/AppLayout.vue';
+import './index.css'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <AppLayout/>
 </template>
 
 <style scoped></style>
---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+
+<h1>This is the layout</h1>
+
+</template>
+<style scoped></style>
